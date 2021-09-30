470b180e7f9cd25f7aed1ac39a9f015a547708b6
Ademario Teles <90646849+ademarioteles@users.noreply.github.com>
2021-09-30 10:36:36 -0300

    Add files via upload
    Exercicio 1002 em java

---
+++ 1002.java
@@ -0,0 +1,22 @@
+
+
+
+import java.io.IOException;
+ import java.util.*;
+
+ 
+public class Main {
+ 
+ 
+	public static void main(String[] args) {
+		
+double pi = 3.14159;
+
+Scanner entrada = new Scanner(System.in);
+
+double raio = entrada.nextDouble();
+
+
+System.out.printf("A=%.4f\n", pi * (raio * raio));
+	}
+}
