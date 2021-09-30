2b37cf1ce873378169a413b7de5a82f15517cebe
Ademario Teles <90646849+ademarioteles@users.noreply.github.com>
2021-09-30 11:06:45 -0300

    Add files via upload

---
+++ 1004.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.*;
+
+
+public class Main{
+	
+	public static void main(String[] args) throws IOException {
+
+		Scanner entrada = new Scanner(System.in);
+		
+
+		int result = entrada.nextInt() * entrada.nextInt();		
+		
+		
+		System.out.println("PROD = " + result);
+	}
+}
