f73ec22df920f2b5082b6db9eff62b6086304542
Ademario Teles <90646849+ademarioteles@users.noreply.github.com>
2021-09-30 10:54:17 -0300

    Add files via upload

---
+++ 1003.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.util.*;
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        
+int x,z;
+
+
+Scanner entrada = new Scanner(System.in);
+
+x = entrada.nextInt();
+z =  entrada.nextInt();
+
+System.out.printf("SOMA = %d\n", x + z);
+
+    }
+ 
+}
