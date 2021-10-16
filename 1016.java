b07685ec5b76d73c6f435e1c05a3e473be6c4202
Ademario Teles <90646849+ademarioteles@users.noreply.github.com>
2021-10-16 16:05:30 -0300

    Create 1016.java

---
+++ 1016.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        
+         
+        int m;
+        Scanner x =  new Scanner(System.in);
+        m = x.nextInt();
+        System.out.println(m * 2 + " minutos");
+ 
+
+    }
+ 
+}
